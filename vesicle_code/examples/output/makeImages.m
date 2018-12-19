e7c96d35c95aa8af80e04cf54a8504325b50525a
Bryan Quaife <bquaife@fsu.edu>
2018-12-19 09:08:58 -0500

    worked on figure 8

--- vesicle_code/examples/output/makeImages.m
+++ vesicle_code/examples/output/makeImages.m
@@ -1,9 +1,9 @@
 addpath ../../src
 set(0,'DefaultAxesFontSize',22)
 
-irate = 100; % controls the speed of the visualization
+irate = 20; % controls the speed of the visualization
 
-if 1
+if 0
 %  file = 'extensional2VesData.bin';
   file = '~/projects/brinkman/vesicle_code/results/extensional2Ves/adR2em1adS1e0_ra040/extensional2VesData.bin';
   ax = [-4 4 -2 2];
@@ -40,8 +40,8 @@ if 0
   ax = [-8 8 -5 5];
   options.confined = false;
 end
-if 0
-  file = 'shear2VesCData.bin.run1';
+if 1
+  file = 'shear2VesBData.bin.run1';
 %  file = '~/projects/brinkman/vesicle_code/results/shear2Ves/adR4em1adS7em1Chi5em1_ra090/shear2VesData.bin';
   ax = [-10 10 -3 3];
   options.confined = false;
