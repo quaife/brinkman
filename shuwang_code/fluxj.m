f7b8e96d343d15d2945e04b1c2a97cb8a85e37bc
Ashley <ag12s@my.fsu.edu>
2020-02-26 14:29:22 -0500

    More comments

--- shuwang_code/fluxj.m
+++ shuwang_code/fluxj.m
@@ -1,51 +1,43 @@
 % THIS ROUTINE COMPUTES THE FORCING ON THE RHS flux OF THE Cahn_Hillard
 % Equation for rcon periodic on [0,1] and for closed curves
 function term = fluxj(sl,theta,rcon,bendsti,bendratio,consta,m,eps_ch)
-% sl - length of vesicle
-% theta - opening angle
-% rcon - concentration
-% bendsti - bending stiffness
-% bendratio - bending ratio
-% consta - term 'a' in the paper. It is the line tension scaled by a
+% sl: length of vesicle
+% theta: opening angle
+% rcon: concentration
+% bendsti: bending stiffness
+% bendratio: bending ratio
+% consta: term 'a' in the paper. It is the line tension scaled by a
 % characteristic bending stiffness
-% m - number of points on vesicle
-% eps_ch - small parameter in equation (65)
+% m: number of points on vesicle
+% eps_ch: small parameter in equation (65)
 
-%term - a/e(f'(u)-e^2u_ss) term in eq (13) + 
+%term: a/e(f'(u)-e^2u_ss) + b'(u)/2 * k^2 terms in eq (13) 
 
-% compute the 1st term from free energy ...?
 
-% Computing the a/e(f'(u)-e^2u_ss) term in eq (13)
-% term1 is f'(u) where f(u) is defined immediately after equation (11).
-% This shows up in equation (13)
-% It is the derivative of the double-well potential
+% dfu is the derivative of the double-well potential
 dfu = 1/2*(rcon(1,1:m).*(ones(1,m)-rcon(1,1:m)).^2 - ...
              rcon(1,1:m).^2.*(ones(1,m)-rcon(1,1:m)));
 % derivative of the concentration
 rcons = fd1(rcon,m); 
 % second derivative of the concentration
 rconss = fd1(rcons,m);
-% This is eps^2 * u_{ss} as defined in equation (13)
+% term2 is eps^2 * u_ss as defined in equation (13)
 term2 = -eps_ch^2*rconss(1,1:m)/sl^2;
 
-%compute the 3rd term
+% computing the b'(u)/2 * k^2 term in eq (13)
 b0 = bendsti;
 b1 = bendsti*bendratio;
-% rbn is not even used after this.
+% rbn is b(u).
 rbn = b0*(ones(1,m) - rcon(1,1:m)) + b1*rcon(1,1:m);
-rbndu = (b1 - b0)*ones(1,m); 
-% difference between the bending ratios
-% Taking a difference seems really strange
+% rbndu is b'(u) 
+rbndu = (b1 - b0)*ones(1,m);
 rbn = [rbn rbn(1)];
-
 % compute the curvature
 dkap = acurv(sl,theta,m);
-
+% term3 is b'(u)/2 * k^2 in eq (13)
 term3 = 0.5*rbndu.*dkap(1,1:m).^2;
-% compute the 4th term compute partial kappa partial rcon here
 
-% now put all terms together. Note that term3 is not multiplied by the
-% a/eps term
+%term is a/e(f'(u)-e^2u_ss) + b'(u)/2 * k^2 terms in eq (13) 
 term(1,1:m) = consta/eps_ch * (dfu + term2) + term3(1,1:m);
 term = [term term(1)];
 
--- shuwang_code/frconim.m
+++ shuwang_code/frconim.m
@@ -13,30 +13,37 @@ function [fncon,termd] = frconim(m,sl,rcon,theta,bendsti,...
 % fncon - ???
 % termd - ???
  
-% compute the old forcing
+%setting up RHS of eq (75) 
 %computing the variational derivative in eq (13) only for
-%the a/e(f'(u)-e^2u_ss) term
+% termd is the a/e(f'(u)-e^2u_ss) + b'(u)/2 * k^2 terms in eq (13) 
 termd = fluxj(sl,theta,rcon,bendsti,bendratio,consta,m,eps_ch);
-%setting up RHS of eq (75)
-% now taking derivative of termd twice
+% now taking derivative of the variational derivative twice
 rcons = fd1(termd,m);       
 rconss = fd1(rcons,m);
-
+%betarcon is the 1/pe term in eq (75)?
 betarcon = 1;
+%fcon is u_t in eq (75)
 fcon(1:m) = betarcon*rconss(1:m)/sl^2;
 fcon(m+1) = fcon(1);
 
-% now we must subtract off the stiffest part in Fourier space.
+% now we must subtract off the stiffest part in Fourier space. (???)
+
+%temp is u_t in eq (75)
 temp(1:m) = fcon(1:m);
+%tempt is u, input parameter
 tempt(1:m) = rcon(1:m);
 
 temp = [temp temp(1)];
 tempt = [tempt tempt(1)];
-temp = fft(temp,m);
-tempt = fft(tempt,m);
 
+%temp is now the fourier transform of u_t
 % this is a fourth order derivative
+temp = fft(temp,m);
+%tempt is now the fourier transform of u
+tempt = fft(tempt,m);
+%N are the fourier modes
 N=pi*2*[0 1:m/2 m/2-1:-1:1];
+%rlen 
 rlen=(N/sl).^4*consta*eps_ch;
 
 temp4 = temp(1,1:m)+rlen(1,1:m).*tempt(1,1:m);
