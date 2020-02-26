0b3092f4939b4374886e2cff926e8a9fb98c3adb
Ashley <ag12s@my.fsu.edu>
2020-02-26 15:14:58 -0500

    Last round of comments for frconim ... for today.

--- shuwang_code/frconim.m
+++ shuwang_code/frconim.m
@@ -13,16 +13,17 @@ function [fncon,termd] = frconim(m,sl,rcon,theta,bendsti,...
 % fncon - ???
 % termd - ???
  
-%setting up RHS of eq (75) 
+%setting up RHS of eq (68) 
 %computing the variational derivative in eq (13) only for
 % termd is the a/e(f'(u)-e^2u_ss) + b'(u)/2 * k^2 terms in eq (13) 
 termd = fluxj(sl,theta,rcon,bendsti,bendratio,consta,m,eps_ch);
 % now taking derivative of the variational derivative twice
 rcons = fd1(termd,m);       
 rconss = fd1(rcons,m);
-%betarcon is the 1/pe term in eq (75)?
+%betarcon is the 1/pe term in eq (67)?
 betarcon = 1;
-%fcon is u_t in eq (75)
+%fcon part of N_2 in eq (67)
+% missing the ae/s_a^4 * u_aaaa term, a is shorthand for alpha here
 fcon(1:m) = betarcon*rconss(1:m)/sl^2;
 fcon(m+1) = fcon(1);
 
@@ -36,22 +37,29 @@ tempt(1:m) = rcon(1:m);
 temp = [temp temp(1)];
 tempt = [tempt tempt(1)];
 
-%temp is now the fourier transform of u_t
+%move to the phase space
+%temp is now the fourier transform of N_2 without the ae/s_a^4 * u_aaaa 
+%term
 % this is a fourth order derivative
 temp = fft(temp,m);
 %tempt is now the fourier transform of u
 tempt = fft(tempt,m);
 %N are the fourier modes
 N=pi*2*[0 1:m/2 m/2-1:-1:1];
-%rlen 
+%rlen is the coefficient on the first term on the RHS of eq (68)
 rlen=(N/sl).^4*consta*eps_ch;
-
+%temp4 is uhat_t(k,t) in eq (68)
 temp4 = temp(1,1:m)+rlen(1,1:m).*tempt(1,1:m);
-
+%Move back real space
 temp4 = real(ifft(temp4,m));
-
+%fncon is u_t in eq (75)
 fncon(1,1:m) = temp4(1,1:m);
 
+%why is this commented?
 %fncon(1,m+1) = fncon(1,1);
 
+%NOTES: 
+%       Not sure where the |k|^4 term went in eq (68)
+%       Not sure where the the 1/pe * ae/s_a^4 * u_aaaa term went eq (67)
+%       Not sure where the - sign went in eq (68)
 end
