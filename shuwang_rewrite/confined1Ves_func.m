84d2b6323fbb682a47e0c5438126e0d20f104d45
ag12s <32399647+ag12s@users.noreply.github.com>
2021-08-25 10:54:59 -0400

    Update confined1Ves_func.m
    Updating driver file for rcc runs

--- shuwang_rewrite/confined1Ves_func.m
+++ shuwang_rewrite/confined1Ves_func.m
@@ -16,7 +16,7 @@ params.N = N; % points on vesicle
 params.Nbd = Nbd; % points on the solid wall
 params.dt = dt; % time step size
 params.T = 0.2; % time horizon
-params.saveRate = 1; % ouptut frequency
+params.saveRate = 10; % ouptut frequency
 params.concentra = concentration; % constant, initial concentration of 
                                   % lipid species
 params.oddeven = -1; % flag for initial lipid species profile
@@ -37,7 +37,7 @@ params.bendratio = b_min/b_max; % ratio between max and min bending stiffness
 params.consta = 100; % parameter 'a' in the Cahn-Hilliard energy
 params.nloop = 20; % number of time steps of Cahn-Hilliard to be taken at 
                    %each time step of the hydrodynamics
-params.epsch = 0.28; % small parameter  in the double-well potential 
+params.epsch = 0.04; % small parameter  in the double-well potential 
 params.gmresTol = 1e-10; %GMRES tolerance
 params.gmresMaxIter = params.N; %maximum number of GMRES iterations
 params.SPcoeff = fluxCoeff; %semi-permeable coefficient
@@ -50,7 +50,7 @@ params.angle = 0;%pi/6; % The tracking point is programmed to be at 0,0.
 options.confined = true; %param for now to pass into tstep, change later
 options.verbose = false;  % write data to console
 options.saveData = true; % save the data
-options.usePlot = true;  % plot the data
+options.usePlot = false;  % plot the data
 options.axis = [-35 0 -4 4];
 options.dataFile = true; % data file name
 options.logFile = true;  % log file name
